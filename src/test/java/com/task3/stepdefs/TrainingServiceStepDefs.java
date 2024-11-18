36b141e488e0c988b8aa287b416b435e28080e4f
jisusoc19 <papati0582@gmail.com>
2024-11-18 17:13:52 -0500

    GYm-Main-Microservice-Cucumber Implementation New Test

--- src/test/java/com/task3/stepdefs/TrainingServiceStepDefs.java
+++ src/test/java/com/task3/stepdefs/TrainingServiceStepDefs.java
@@ -24,8 +24,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -38,6 +37,9 @@ public class TrainingServiceStepDefs {
     private String exceptionMessage;
     private Training training;
     private TrainingDtoMicroServiceTaskMicro savedTraining;
+
+
+    private Exception expectedException;
 
     @Given("new Training with valid information")
     public void new_training_with_valid_information() throws ParseException {
@@ -77,26 +79,96 @@ public class TrainingServiceStepDefs {
     }
 
     @Given("Save Training with bad credentials")
-    public void save_training_with_bad_credentials() {
+    public void save_training_with_bad_credentials() throws ParseException {
         training = new Training();
         training.setTrainingName(null);
+        training.setTrainer_id(new Trainer());
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+        Date trainingDate = sdf.parse("2024-10-28T21:00:30.325Z");
+        training.setTraining_date(trainingDate);
     }
 
-    @When("user tries to save Training with invalid information")
+    @When("user try to save Training with unvalid information Trainingname null")
     public void user_tries_to_save_training_with_invalid_information() {
-        try {
-            savedTraining = trainingService.save(training);
-            exceptionMessage = null;
-        } catch (Exception ex) {
-            exceptionMessage = ex.getMessage();
-        }
+        expectedException = assertThrows(IllegalArgumentException.class, () -> {
+            trainingService.save(training);
+        });
     }
+
+
 
     @Then("training doesn't save and throws an error")
     public void training_doesnt_save_and_throws_an_error() {
-        assertNull(savedTraining);
-        assertNotNull(exceptionMessage);
+        assertNotNull(expectedException);
+        assertEquals("Training name cannot be null", expectedException.getMessage());
     }
+
+    @Given("Save Null Training")
+    public  void save_Null_Training(){
+        Training traininnull = null;
+    }
+    @When("Training Save With Null")
+    public void Training_Save_With_Null(){
+        trainingService.save(null);
+    }
+    @Then("Training throw a NullPointerExeption")
+    public void Training_throw_a_NullPointerExeption(){
+        assertNotNull(expectedException);
+        assertTrue(expectedException instanceof NullPointerException);
+    }
+
+    @When("Training Save With Null")
+    public void training_Save_With_Null() {
+        expectedException = assertThrows(NullPointerException.class, () -> {
+            trainingService.save(training);
+        });
+    }
+
+    @Then("Training throws a NullPointerException")
+    public void training_throws_a_NullPointerException() {
+        assertNotNull(expectedException);
+        assertTrue(expectedException instanceof NullPointerException);
+    }
+
+    @Given("Update Existing Training by Username")
+    public void update_existing_training_by_username() {
+        TrainingDtoMicroServiceTaskMicro training =new TrainingDtoMicroServiceTaskMicro();
+    }
+
+    @When("Training tries to find an existing Training to update Status")
+    public void training_tries_to_find_an_existing_training_to_update_status() {
+        // Suponemos que la acción de encontrar y luego actualizar es correcta, gestionada en este paso
+        TrainingDtoMicroServiceTaskMicro training = new TrainingDtoMicroServiceTaskMicro();
+        when(trainingService.UpdateActionType("PEPITO","Add")).thenReturn(training);
+    }
+
+    @Then("Training updates successfully")
+    public void training_updates_successfully() {
+        TrainingDtoMicroServiceTaskMicro updatedTraining = trainingService.UpdateActionType("PEPITO","ADD");
+        assertNotNull(updatedTraining);
+        assertEquals("Active", updatedTraining.getActionType());
+    }
+
+
+    @Given("Update Existing Training by a null id")
+    public void update_existing_training_by_a_null_id() {
+        when(trainingService.findbyid(null)).thenThrow(new NullPointerException("Training ID cannot be null"));
+    }
+
+    @When("Training tries to find a wrong Training to update Status")
+    public void training_tries_to_find_a_wrong_training_to_update_status() {
+
+        expectedException = assertThrows(NullPointerException.class, () -> {
+            trainingService.findbyid(null);
+        });
+    }
+
+    @Then("Training throws a NullPointerException")
+    public void training_throws_a_null_pointer_exception() {
+        assertNotNull(expectedException);
+        assertEquals("Training ID cannot be null", expectedException.getMessage());
+    }
+
 
 
 }
--- src/test/resources/features/training.feature
+++ src/test/resources/features/training.feature
@@ -6,11 +6,30 @@ Feature: TrainingService implement Save Entity a send it to other Microservice
     When user try to save this Training with valid information
     Then Training save succesfully and recive a confirmation message
 
-  Scenario: save new Training with null
+  Scenario: save new Training bad Credentials
     Given Save Training with bad Credentials
-    When user try to save Training with unvalid information
-   Then training dont save a throw a error
+    When user try to save Training with unvalid information Trainingname null
+    Then training dont save a throw a error
 
+  Scenario:Save new Training with Null Username
+  Given Save Null Training
+  When Training Save With Null
+  Then Training throw a NullPointerExeption
+
+  Scenario:Save new Training with Null
+    Given Save Null Training
+    When Training Save With Null User
+    Then Training should return
+
+  Scenario Update Existing Training Status
+    Given Update Existing Training by Username
+    When Training try to find a existing Training to update Status
+    Then Training update succesfully
+
+  Scenario Update Existing Training Status Failed
+    Given Update Existing Training by a null Username
+    When Training try to find an worng Training to update Status
+    Then Training throw a NullPointerExeption
     ##Given es lo que quiero probar, entonces ofresco un contexto
   ##when lo que hace es lo que quiero hacer y lo que me llega (Las credenciales que tengo)
   ##then es lo que espero que suceda
